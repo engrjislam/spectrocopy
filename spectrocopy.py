aaa92771ba5c405db4b4e2b4413c1740d9b19643
Johirul Islam <johirul.islam@engineer.com>
2017-12-20 02:45:41 +0200

    Update spectrocopy.py

--- spectrocopy.py
+++ spectrocopy.py
@@ -1,7 +1,9 @@
 '''
-@filename: spectroscopy
-@author: Johirul Islam
+@filename: spectroscopy.py
+@author: Johirul Islam 
+@email: engr.jislam@gmail.com
 @created: 05.11.2017
+@updated: 20.12.2017
 @description: 
     A very simple graphical user interface to plot spectroscopy.
     
@@ -26,12 +28,15 @@
 '''
 
 
+from matplotlib.widgets import TextBox
+from os import walk
 from tkinter import *
 from tkinter import filedialog, messagebox
-from os import walk
-import numpy as np
+
 import matplotlib.pyplot as plt
-import os
+import numpy as np
+import os, statistics
+
 
 class Spectroscopy():
     """
@@ -41,13 +46,10 @@ class Spectroscopy():
     print(__name__, __doc__)
     
     #class valribles
-    widthpixels = 210 
-    heightpixels = 105
-    title = "spectrocopy"
-    x_min = 0.0
-    x_max = 100.0
-    required_extension = '.txt'
-    data = []
+    widthpixels, heightpixels, selected_data_index, count_index = 340, 165, 0, 0
+    title, required_extension, intensity_info = "Spectrocopy", '.txt', ['', '']
+    data, binding_energies, intensities, selected_data_indices = [], [], [], []
+    fig, ax, text_box, selected, line, pick_event, key_press_event = None, None, None, [None, None, None, None], None, None, None
     
     def __init__(self, gui):
         '''
@@ -55,20 +57,20 @@ class Spectroscopy():
         Spectroscopy initialized successfully!! ...
         '''
         
-        self.setWindowTitle(gui)
-        self.setNonResizableWindow(gui)
-        self.x_ranges(gui)
-        self.setLoadButtons(gui)
-        self.setWindowAtCenter(gui)
+        self.set_window_title(gui)
+        self.set_non_resizable_window(gui)
+        self.set_instructions(gui)
+        self.set_buttons(gui)
+        self.set_window_at_center(gui)
         
         
         print(self.__init__.__name__, self.__init__.__doc__)
         
-    def setWindowAtCenter(self, gui):        
+    def set_window_at_center(self, gui):        
         '''
         Placing the window into the center of the screen ...
         '''
-        print(self.setWindowAtCenter.__name__, self.setWindowAtCenter.__doc__)
+        print(self.set_window_at_center.__name__, self.set_window_at_center.__doc__)
         
         gui.update_idletasks()
         w = gui.winfo_screenwidth()
@@ -78,96 +80,48 @@ class Spectroscopy():
         y = h/2 - size[1]/2
         gui.geometry("%dx%d+%d+%d" % (size + (x, y)))
         
-    def setWindowTitle(self, gui):
+    def set_window_title(self, gui):
         '''
         Setting the window title ...
         '''
-        print(self.setWindowTitle.__name__, self.setWindowTitle.__doc__)
+        print(self.set_window_title.__name__, self.set_window_title.__doc__)
         gui.title(self.title)
         
-    def setNonResizableWindow(self, gui):
+    def set_non_resizable_window(self, gui):
         '''
         Setting the window to non resizable ...
         '''
-        print(self.setNonResizableWindow.__name__, self.setNonResizableWindow.__doc__)
+        print(self.set_non_resizable_window.__name__, self.set_non_resizable_window.__doc__)
         
         gui.geometry('{}x{}'.format(self.widthpixels, self.heightpixels))
         gui.resizable(width=False, height=False)
         
-    def setLoadButtons(self, gui):
+    def set_instructions(self, gui):
+        '''
+        Placing instructions ... 
+        '''
+        print(self.set_instructions.__name__, self.set_instructions.__doc__)
+        
+        # set instructions for user
+        Label(gui, text="Instructions:").grid(row=0, columnspan = 2, sticky=W, padx=5, pady=5)
+        Label(gui, text="(1) Click 'Load Data' button and specify spectrum data folder.").grid(row=1, columnspan = 2, sticky=W, padx=5, pady=5)
+        Label(gui, text="(2) Click 'Plot Data' button to plot spectral data in a window.").grid(row=2, columnspan = 2, sticky=W, padx=5, pady=5)
+        Label(gui, text="(3) Finally choose 2 sets of x (binding energies) values.").grid(row=3, columnspan = 2, sticky=W, padx=5, pady=5)
+        
+    def set_buttons(self, gui):
         '''
         Placing Load Data & Plot Data Buttons ...
         '''
-        print(self.setLoadButtons.__name__, self.setLoadButtons.__doc__)
+        print(self.set_buttons.__name__, self.set_buttons.__doc__)
         
-        Button(gui, text="Load Data", command=lambda:self.loaddata()).grid(row=4, column=0, sticky=W, padx=5, pady=5)
-        Button(gui, text="Plot Data", command=self.plotdata).grid(row=4, column=1, sticky=E, padx=5, pady=5)
-        
-    def x_ranges(self, gui):
-        '''
-        Placing x ranges ... 
-        '''
-        print(self.x_ranges.__name__, self.x_ranges.__doc__)
-        
-        # set x-min & x-max label into the gui
-        Label(gui, text="x-min").grid(row=0, column = 0, padx=5, pady=5)
-        Label(gui, text="x-max").grid(row=2, column = 0, padx=5, pady=5)
-        
-        # validate x-min & x-max
-        v_x_min = gui.register(self.validate_x_min) 
-        v_x_max = gui.register(self.validate_x_max) 
-        
-        # set x-min & x-max input field
-        self.x_min_input = Entry(gui, validate="key", validatecommand=(v_x_min, '%P'))
-        self.x_max_input = Entry(gui, validate="key", validatecommand=(v_x_max, '%P'))
-        
-        # set x ranges according to x-min & x-max
-        self.x_min_input.insert(10, self.x_min)
-        self.x_max_input.insert(10, self.x_max)
-        
-        # show x-min & x-max input field
-        self.x_min_input.grid(row=0, column=1, padx=5, pady=5)
-        self.x_max_input.grid(row=2, column=1, padx=5, pady=5)
-        
-    def validate_x_min(self, x_min):
-        '''
-        Validating x-min ... 
-        '''
-        print(self.validate_x_min.__name__, self.validate_x_min.__doc__)
-        
-        # set x-min to 0.0 for empty input
-        if not x_min: 
-            self.x_min = 0.0
-            return True
-        
-        try:
-            self.x_min = float(x_min)
-            return True
-        except ValueError:
-            return False
-        
-    def validate_x_max(self, x_max):
-        '''
-        Validating x-max ... 
-        '''
-        print(self.validate_x_max.__name__, self.validate_x_max.__doc__)
-        
-        # set x-max to 100.0 for empty input
-        if not x_max: 
-            self.x_max = 100.0
-            return True
-        
-        try:
-            self.x_max = float(x_max)
-            return True
-        except ValueError:
-            return False
-
-    def loaddata(self):
+        Button(gui, text="Load Data", command=lambda:self.load_data()).grid(row=4, column=0, sticky=W, padx=5, pady=5)
+        Button(gui, text="Plot Data", command=self.plot_data).grid(row=4, column=1, sticky=E, padx=5, pady=5)
+    
+    def load_data(self):
         '''
         loading directory chooser ...
         '''
-        print(self.loaddata.__name__, self.loaddata.__doc__)
+        print(self.load_data.__name__, self.load_data.__doc__)
         
         directory = filedialog.askdirectory(title = "Select Directory")
         if directory: 
@@ -225,75 +179,159 @@ class Spectroscopy():
                         tuples.append(temporary_tuple)
         return tuples
     
-    def plotdata(self):
+    def plot_data(self):
         '''
         Ploting data ...
         '''
-        print(self.plotdata.__name__, self.plotdata.__doc__)
+        print(self.plot_data.__name__, self.plot_data.__doc__)
         
         if len(self.data) == 0:
             messagebox.showerror("Failed!", "No data loaded!!")
         else:
             # print([x for (x, y) in self.data])
             sorted(self.data)
-            self.x_min, self.x_max = min(self.x_min, self.x_max), max(self.x_min, self.x_max)
             
-            binding_energies = [b for (b, i) in self.data[0:500]]
-            intensities = [0 for x in range(0, 500)]
-            for x in range(0, len(self.data)):
-                intensities[x % 500] += self.data[x][1]
-                
-            """
-            x1, y1, x2, y2 = binding_energies[0], intensities[0], binding_energies[-1], intensities[-1]
-            for x in range(0, len(intensities)):
-                '''
-                Assume, A(x1, y1) and B(x2, y2) forms straight line starting. Let P(x, y) is a point 
-                on line AB and devide AB as ratio of AP:BP = m:n. So, 
-                    x = (m*x2 + n*x1) / (m + n)
-                    y = (m*y2 + n*y1) / (m + n)
-                '''
-                m, n = x + 1, 500 - (x + 1)
-                y = (m*y2 + n*y1) / (m + n)
-                intensities[x] -= intensities[x] - y
-            """
+            # remove background noises
+            self.remove_noise()
             
-            # check in range
-            r_binding_energies = []
-            r_intensities = []
-            for x in range(0, len(binding_energies)):
-                if binding_energies[x] >= self.x_min and binding_energies[x] <= self.x_max:
-                    r_binding_energies.append(binding_energies[x])
-                    r_intensities.append(intensities[x])
-            binding_energies, intensities = r_binding_energies, r_intensities
+            # spectrum in the given range
+            self.binding_energies, self.intensities = self.spectrum_in_range()
             
             # window title    
-            plt.figure('Binding energy (eV) VS Intensity (arbitrary units)')
+            self.fig = plt.figure('Binding energy (eV) VS Intensity (arbitrary units)')
+            self.ax = self.fig.add_subplot(111)
+            plt.subplots_adjust(bottom=0.20) # postion of text box -- how far from original graph
+            
             # figure title
-            plt.title('Binding energy (eV) VS Intensity (arbitrary units)')
-            plt.xlabel('Binding energy (eV)')
-            plt.ylabel('Intensity (arbitrary units)')
-            plt.plot(binding_energies, intensities)
+            self.ax.set_title('Binding energy (eV) VS Intensity (arbitrary units)')
+            self.ax.set_xlabel('Binding energy (eV)')
+            self.ax.set_ylabel('Intensity (arbitrary units)')
+            self.line, = self.ax.plot(self.binding_energies, self.intensities, '-', picker=5)  # 5 points tolerance
+            
+            self.text_box = self.set_text_box()
+            self.selected[0], = self.ax.plot([self.binding_energies[0]], [self.intensities[0]], 'o', ms=12, alpha=0.4, color='#123abc', visible=False)
+            self.selected[1], = self.ax.plot([self.binding_energies[0]], [self.intensities[0]], 'o', ms=12, alpha=0.4, color='#123abc', visible=False)
+            self.selected[2], = self.ax.plot([self.binding_energies[0]], [self.intensities[0]], 'o', ms=12, alpha=0.4, color='#abc456', visible=False)
+            self.selected[3], = self.ax.plot([self.binding_energies[0]], [self.intensities[0]], 'o', ms=12, alpha=0.4, color='#abc456', visible=False)
+            
+            self.pick_event = self.fig.canvas.mpl_connect('pick_event', self.onpick)
+            self.key_press_event = self.fig.canvas.mpl_connect('key_press_event', self.onpress)
+            
             plt.show()
         
-        
-    def is_downward(self, x1, y1, x2, y2):
+    def find_slope(self, x1, y1, x2, y2):
         try:
-            slope = (y2 - y1) / (x2 - x1)
-            if slope < 0:
-                return True
-            else: 
-                return False
+            return (y2 - y1) / (x2 - x1)
         except(ValueError, ZeroDivisionError):
-            return False
+            return None
+            
+    def remove_noise(self):
+        '''
+        Removing noises ...
+        '''
+        
+        first_binding_energy, last_binding_energy = self.data[0][0], self.data[-1][0]
+        first_intensity, last_intensity = self.data[0][1], self.data[-1][1]
+        slope = self.find_slope(first_binding_energy, first_intensity, last_binding_energy, last_intensity)
+        
+        '''
+        y = m(x - x0) + y0
+        here, m is slope
+        '''
+        for index in range(len(self.data)):
+            (binding_energy_on_line, intensity_with_noise) = self.data[index]
+            intensity_on_line = slope * (binding_energy_on_line - first_binding_energy) + first_intensity
+            self.data[index] = (binding_energy_on_line, intensity_with_noise - intensity_on_line)
+            
+    def spectrum_in_range(self):
+            
+        binding_energies = [b for (b, i) in self.data[0:500]]
+        intensities = [0 for x in range(0, 500)] # intializing 0's
+        
+        for x in range(0, len(self.data)):
+            intensities[x % 500] += self.data[x][1]
+            
+        # check in range
+        r_binding_energies = []
+        r_intensities = []
+        for x in range(0, len(binding_energies)):
+            r_binding_energies.append(binding_energies[x])
+            r_intensities.append(intensities[x])
+        return r_binding_energies, r_intensities
+        
+    def set_text_box(self):
+        axbox = plt.axes([0.125, 0.01, 0.775, 0.075])
+        text_box = TextBox(axbox, '', initial='')
+        return text_box
         
     def calculate_area(self, x_coordinates, y_coordinates):
-        '''
-        calculate area of trapizoid
-        '''
         return np.trapz(y_coordinates, x=x_coordinates)
+   
+    def onpress(self, event):
+    
+        if self.selected_data_index is None:
+            return
+        if event.key not in ('n', 'p'):
+            return
+        if event.key == 'n':
+            inc = 1
+        else:
+            inc = -1
+
+        self.selected_data_index += inc
+        self.selected_data_index = np.clip(self.selected_data_index, 0, len(self.binding_energies) - 1)
+        self.update()
+
+    def onpick(self, event):
         
-   
-   
+        if event.artist != self.line:
+            return True
+
+        N = len(event.ind)
+        if not N:
+            return True
+
+        # the click locations
+        x = event.mouseevent.xdata
+        y = event.mouseevent.ydata
+        
+        middle_index = statistics.median_high(event.ind)        
+        #distances = min([np.hypot(x - self.binding_energies[index], y - self.intensities[index]) for index in event.ind])
+        #indmin = distances.argmin()
+        #data_index = event.ind[indmin]
+
+        self.selected_data_index = middle_index
+        self.update()
+
+    def update(self):
+    
+        if self.selected_data_index is None:
+            return
+        
+        data_index = self.selected_data_index
+        self.selected_data_indices.append(data_index)
+        
+        self.selected[self.count_index].set_visible(True)
+        self.selected[self.count_index].set_data(self.binding_energies[data_index], self.intensities[data_index])
+        
+        self.count_index += 1
+        self.fig.canvas.draw()
+        
+        if self.count_index == 4:
+            start, end = self.selected_data_indices[0], self.selected_data_indices[1]
+            intensity_1 = self.calculate_area(self.binding_energies[start:end+1], self.intensities[start:end+1])
+            
+            start, end = self.selected_data_indices[2], self.selected_data_indices[3]
+            intensity_2 = self.calculate_area(self.binding_energies[start:end+1], self.intensities[start:end+1])
+            
+            intensity_ratio = intensity_1 / intensity_2
+            
+            self.text_box.set_val('intensity_1 = {:.2f}; and intensity_2 = {:.2f} \nintensity_1 / intensity_2 = {:.2f}'.format(intensity_1, intensity_2, intensity_ratio))
+            
+            self.fig.canvas.mpl_disconnect(self.pick_event)
+            self.fig.canvas.mpl_disconnect(self.key_press_event)
+
+        
 if __name__ == "__main__":
     root = Tk()
     
